b90f0c762d4665c6263db47cbe96fc1e4059fdc0
gbach415 <g.bachmann1@gmail.com>
2022-02-22 12:46:42 -0500

    Setting up main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,43 @@
-// main.py
+"""
+CSCI.320 Project: Movie Domain
+
+Authors: - Milo Berry
+         - Gunnar Bachmann
+         - Ari Wisenburn
+         - Noah Pelletier
+
+Description:
+"""
+
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+
+
+# TODO storing credentials. environment variables, gitignore a config file/folder, etc...
+username = "YOUR_CS_USERNAME"
+password = "YOUR_CS_PASSWORD"
+dbName = "YOUR_DB_NAME"
+
+
+try:
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('localhost', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+
+        conn = psycopg2.connect(**params)
+        curs = conn.cursor()
+        print("Database connection established")
+        conn.close()
+except:
+    print("Connection failed")
