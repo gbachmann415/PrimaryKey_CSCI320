c0045c21e9d2804f15d014dd05b196248c64f98b
gbach415 <g.bachmann1@gmail.com>
2022-03-15 15:23:09 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -20,46 +20,56 @@ username = DB_USERNAME
 password = DB_PASSWORD
 dbName = DB_NAME
 
-"""
-Establish a connection to the database with username, password, and database name.
-Failed connection will result in a connection failed output to the user.
-"""
-try:
-    print("Attempting to establish database connection...")
-    # with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
-    #                         ssh_username=username,
-    #                         ssh_password=password,
-    #                         remote_bind_address=('localhost', 5432)) as server:
-    #     server.start()
-    #     print("SSH tunnel established")
-    #     params = {
-    #         'database': dbName,
-    #         'user': username,
-    #         'password': password,
-    #         'host': 'localhost',
-    #         'port': server.local_bind_port
-    #     }
-    #
-    #     conn = psycopg2.connect(**params)
-    #     curs = conn.cursor()
-    #     print("Database connection established")
-    #     conn.close()
-except:
-    print("Connection failed")
-    # TODO do we want to end program if connection fails by adding an exit statement?
 
+def connect_to_db():
+    """
+    Establish a connection to the database with username, password, and database name.
+    Failed connection will result in a connection failed output to the user and program exit.
+
+    :return: conn = connection to database
+    """
+    try:
+        print("Attempting to establish database connection...")
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=username,
+                                ssh_password=password,
+                                remote_bind_address=('localhost', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            params = {
+                'database': dbName,
+                'user': username,
+                'password': password,
+                'host': 'localhost',
+                'port': server.local_bind_port
+            }
+
+            conn = psycopg2.connect(**params)
+            curs = conn.cursor()
+            print("Database connection established")
+    except:
+        print("Connection failed")
+        exit()
+
+    return conn
+
+def close_connection_to_db(conn):
+    conn.close()
+    return
 
 def main():
     """
     [Description goes here]
     :return: None
     """
-    #
-    # body of main function will go before the connection close call.
-    #
-    ui.MovieApplication().run()
-    conn.close()  # Close the connection to the database
+    # Establish connection to the Database
+    # conn = connect_to_db()
 
+    # Start the Movie Application (Launching the UI)
+    ui.MovieApplication().run()
+
+    # Close the connection to the Database
+    # close_connection_to_db(conn)
 
 if __name__ == '__main__':
     main()
