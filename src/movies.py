91974d2059e8cfebef01808dfb5d77ef33acda4b
gbach415 <g.bachmann1@gmail.com>
2022-03-16 16:23:35 -0400

    add DB connection functions to file

--- src/movies.py
+++ src/movies.py
@@ -11,6 +11,47 @@ Authors: - Milo Berry
 Description:
 """
 
+from config import DB_USERNAME, DB_PASSWORD, DB_NAME
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+
+
+def connect_to_db():
+    """
+    Establish a connection to the database with username, password, and database name.
+    Failed connection will result in a connection failed output to the user and program exit.
+
+    :return: conn = connection to database
+    """
+    try:
+        server = SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                    ssh_username=DB_USERNAME,
+                                    ssh_password=DB_PASSWORD,
+                                    remote_bind_address=('localhost', 5432))
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': DB_NAME,
+            'user': DB_USERNAME,
+            'password': DB_PASSWORD,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+        conn = psycopg2.connect(**params)
+        print("Database connection established")
+    except:
+        print("Connection failed")
+        exit()
+
+    return conn
+
+
+def close_connection_to_db(conn):
+    conn.close()
+    print("Database connection closed")
+    return
+
 
 def get_movie(movie_id, username):
   """
