d677abd567eeceb4123b7ccbb3ec87ddddd1e6ce
gbach415 <g.bachmann1@gmail.com>
2022-04-11 19:23:09 -0400

    Update load_test_data.py

--- src/load_test_data.py
+++ src/load_test_data.py
@@ -28,84 +28,84 @@ collection6_movies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 17, 18, 20]
 def main():
     # Create users
     print("Creating Users...")
-    print(create_user('test1', 'test1pw', 'Gunnar', 'Bachmann', 'test1@gmail.com'))
-    print(create_user('test2', 'test2pw', 'Bob', 'Dylan', 'test2@gmail.com'))
-    print(create_user('test3', 'test3pw', 'Jonny', 'Appleseed', 'test3@gmail.com'))
-    print(create_user('test4', 'test4pw', 'Sam', 'Hunt', 'test4@gmail.com'))
+    print(create_user('test5', 'test5pw', 'Gunnar2', 'Bachmann2', 'test5@gmail.com'))
+    print(create_user('test6', 'test6pw', 'Bob2', 'Dylan2', 'test6@gmail.com'))
+    print(create_user('test7', 'test7pw', 'Jonny2', 'Appleseed2', 'test7@gmail.com'))
+    print(create_user('test8', 'test8pw', 'Sam2', 'Hunt2', 'test8@gmail.com'))
     print('Done\n')
 
     # Follow users
     print("Following Users...")
-    follow_user('test1', 'test2')
-    follow_user('test1', 'test3')
-    follow_user('test1', 'test4')
-    follow_user('test2', 'test1')
-    follow_user('test3', 'test2')
-    follow_user('test4', 'test3')
+    follow_user('test5', 'test6')
+    follow_user('test5', 'test7')
+    follow_user('test5', 'test8')
+    follow_user('test6', 'test5')
+    follow_user('test7', 'test6')
+    follow_user('test8', 'test7')
     print("Done\n")
 
     # Add Collections
     print("Creating Collections...")
-    add_collection('test1', 'Collection1')
-    add_collection('test1', 'Collection2')
-    add_collection('test2', 'Collection3')
-    add_collection('test3', 'Collection4')
-    add_collection('test4', 'Collection5')
-    add_collection('test4', 'Collection6')
+    add_collection('test5', 'Collection7')
+    add_collection('test5', 'Collection8')
+    add_collection('test6', 'Collection9')
+    add_collection('test7', 'Collection10')
+    add_collection('test8', 'Collection11')
+    add_collection('test8', 'Collection12')
     print("Done\n")
 
     # Add Movies to Collections
     print("Adding Movies to Collections...")
     for i in collection1_movies:
-        add_movie_to_collection('test1', 'Collection1', i)
+        add_movie_to_collection('test5', 'Collection7', i)
     for i in collection2_movies:
-        add_movie_to_collection('test1', 'Collection2', i)
+        add_movie_to_collection('test5', 'Collection8', i)
     for i in collection3_movies:
-        add_movie_to_collection('test2', 'Collection3', i)
+        add_movie_to_collection('test6', 'Collection9', i)
     for i in collection4_movies:
-        add_movie_to_collection('test3', 'Collection4', i)
+        add_movie_to_collection('test7', 'Collection10', i)
     for i in collection5_movies:
-        add_movie_to_collection('test4', 'Collection5', i)
+        add_movie_to_collection('test8', 'Collection11', i)
     for i in collection6_movies:
-        add_movie_to_collection('test4', 'Collection6', i)
+        add_movie_to_collection('test8', 'Collection12', i)
     print("Done\n")
 
     # Watch Movies
     print("Marking Movies as Watched...")
     for i in collection1_movies:
-        watch_movie(i, 'test1')
+        watch_movie(i, 'test5')
         sleep(5)
     for i in collection2_movies:
-        watch_movie(i, 'test1')
+        watch_movie(i, 'test5')
         sleep(5)
     for i in collection3_movies:
-        watch_movie(i, 'test2')
+        watch_movie(i, 'test6')
         sleep(5)
     for i in collection4_movies:
-        watch_movie(i, 'test3')
+        watch_movie(i, 'test7')
         sleep(5)
     for i in collection5_movies:
-        watch_movie(i, 'test4')
+        watch_movie(i, 'test8')
         sleep(5)
     for i in collection6_movies:
-        watch_movie(i, 'test4')
+        watch_movie(i, 'test8')
         sleep(5)
     print("Done\n")
 
     # Rate Movies
     print("Rating Movies...")
     for i in collection1_movies:
-        rate_movie(i, 'test1', randint(1, 5))
+        rate_movie(i, 'test5', randint(1, 5))
     for i in collection2_movies:
-        rate_movie(i, 'test1', randint(1, 5))
+        rate_movie(i, 'test5', randint(1, 5))
     for i in collection3_movies:
-        rate_movie(i, 'test2', randint(1, 5))
+        rate_movie(i, 'test6', randint(1, 5))
     for i in collection4_movies:
-        rate_movie(i, 'test3', randint(1, 5))
+        rate_movie(i, 'test7', randint(1, 5))
     for i in collection5_movies:
-        rate_movie(i, 'test4', randint(1, 5))
+        rate_movie(i, 'test8', randint(1, 5))
     for i in collection6_movies:
-        rate_movie(i, 'test4', randint(1, 5))
+        rate_movie(i, 'test8', randint(1, 5))
     print("Done\n")
 
     print("Loading test data has completed.")
